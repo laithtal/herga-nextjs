490383cd56aef00e843abef887be8952ac5e6878
laithtal <laltal.dev@gmail.com>
2023-12-03 13:41:53 +0300

    add responsive

--- src/app/components/Home/HomePage.tsx
+++ src/app/components/Home/HomePage.tsx
@@ -3,34 +3,20 @@ import './HomePage.css'
 
 export const HomePage = ({ data }) => {
 
-
-
-  const styles = {
-    paperContainer: {
-      backgroundImage: `url('https://lmdev.wundeve.com/getmedia/11689c62-33d2-47e2-aa2e-c1cd8f7cc4a0/AlUla-(28).png?width=1920&height=945&ext=.png')`
-    }
-  };
-
   return (
-    <div className='container' style={styles.paperContainer}>
+    <>
       {data &&
-
-        <div className='section-home'>
-
-          <div className="content-l">
-            <p>HOME</p>
-            <div className='arrow'></div>
-            <p>{data.NodeAlias}</p>
+        <div className='container' >
+          <div className='section-home'>
+            <div className="content-l">
+              <p>HOME</p>
+              <div className='arrow'></div>
+              <p>{data.NodeAlias}</p>
+            </div>
+            <div className='hegra'><p>{data.NodeAlias}</p></div>
           </div>
-          <p className='hegra'>{data.NodeAlias}</p>
-
-        </div>
-
-      }
-
-
-
-    </div>
+        </div>}
+    </>
   );
 };
 
